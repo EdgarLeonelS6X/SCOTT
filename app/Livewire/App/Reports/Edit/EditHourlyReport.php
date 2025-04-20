4bd8f3797ee50e1b4ae1d1293b62d8c1422307bf
Edgar Leonel Acevedo Cuevas <edgarleonnels6x@gmail.com>
2025-04-20 14:45:23 -0600

    Feat: Add EditFunctionsReport, EditHourlyReport, and EditMomentlyReport components with corresponding Blade views

---
+++ app/Livewire/App/Reports/Edit/EditHourlyReport.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Reports\Edit;
+
+use Livewire\Component;
+
+class EditHourlyReport extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.reports.edit.edit-hourly-report');
+    }
+}
