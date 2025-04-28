cdab1166fa12f757ec6b6a24172818ae43261c5f
Edgar Leonel Acevedo Cuevas <edgarleonnels6x@gmail.com>
2025-04-28 16:34:06 -0600

    Feat: Improve layout and styling in create momentary report component for better usability and responsiveness

--- resources/views/livewire/app/reports/create/create-momently-report.blade.php
+++ resources/views/livewire/app/reports/create/create-momently-report.blade.php
@@ -12,33 +12,34 @@ use App\Enums\ChannelReviewer;
             firstEdit: true,
             categoryName: @entangle('reportData.category').defer || '',
         }">
-            <div class="p-6 border bg-white dark:bg-gray-800 rounded-2xl shadow-2xl">
-                <div class="flex items-center justify-between cursor-pointer" @click="if (!editingName) open = !open">
-                    <div class="flex items-center">
-                        <button type="button" class="text-primary-600 mr-3" @click.stop="open = !open">
+            <div class="p-4 md:p-6 border bg-white dark:bg-gray-800 rounded-2xl shadow-2xl">
+                <div class="flex flex-wrap items-center justify-between cursor-pointer"
+                    @click="if (!editingName) open = !open">
+                    <div class="flex items-center flex-wrap gap-3">
+                        <button type="button" class="text-primary-600" @click.stop="open = !open">
                             <i :class="open ? 'fas fa-chevron-down' : 'fas fa-chevron-right'"></i>
                         </button>
                         <div class="dark:text-white text-lg font-semibold relative">
                             <h3 title="{{ __('Click here to edit the category name') }}" x-show="!editingName"
                                 @click.stop="editingName = true; 
-                                             if (firstEdit) { categoryName = ''; firstEdit = false; }"
-                                class="cursor-pointer px-3 py-2 rounded-full shadow-md flex items-center gap-2 transition bg-gray-50 border dark:bg-gray-700 dark:border-white">
-                                <i
-                                    class="fa-solid fa-pen text-gray-800 dark:text-gray-200 transition duration-200">...</i>
+                         if (firstEdit) { categoryName = ''; firstEdit = false; }"
+                                class="cursor-pointer px-3 py-2 rounded-full shadow-md flex items-center gap-2 transition bg-gray-50 border dark:bg-gray-700 dark:border-white w-full max-w-[270px] md:max-w-lg truncate">
+                                <i class="fa-solid fa-pen text-gray-800 dark:text-gray-200"></i>
                                 <span x-text="categoryName"></span>
                             </h3>
                             <x-input x-show="editingName" x-model="categoryName" @click.stop
                                 @click.away="editingName = false; 
-                                             if (categoryName.trim() === '') { categoryName = ''; } 
-                                             $wire.set('reportData.category', categoryName);"
+                         if (categoryName.trim() === '') { categoryName = ''; } 
+                         $wire.set('reportData.category', categoryName);"
                                 @keydown.enter.prevent="editingName = false; 
-                                                        if (categoryName.trim() === '') { categoryName = ''; } 
-                                                        $wire.set('reportData.category', categoryName);"
+                                    if (categoryName.trim() === '') { categoryName = ''; } 
+                                    $wire.set('reportData.category', categoryName);"
                                 placeholder="{{ __('Category name') }}" autofocus />
                         </div>
                     </div>
-                    <div class="flex items-center gap-3">
-                        <span class="ml-4 bg-primary-100 text-primary-800 text-sm font-medium py-1 px-2 rounded-full">
+                    <div class="flex items-center gap-3 transition-all duration-300"
+                        :class="(categoryName.length > 15 || editingName) ? 'mt-4 sm:mt-0' : 'mt-0'">
+                        <span class="bg-primary-100 text-primary-800 text-sm font-medium py-1 px-3 rounded-full">
                             {{ __('Contains') }} {{ $this->getChannelCount(0) }}
                             {{ $this->getChannelCount(0) === 1 ? __('channel') : __('channels') }}
                         </span>
@@ -46,15 +47,14 @@ use App\Enums\ChannelReviewer;
                 </div>
                 <div x-show="open" class="mt-6 space-y-6">
                     @foreach ($reportData['channels'] as $channelIndex => $channel)
-                        <div class="p-6 bg-gray-50 border dark:bg-gray-700 rounded-xl shadow-2xl">
-                            <div class="flex justify-between items-center mb-4">
+                        <div class="p-4 md:p-6 bg-gray-50 border dark:bg-gray-700 rounded-xl shadow-2xl">
+                            <div class="flex flex-wrap justify-between items-center mb-4 gap-4">
                                 <h4 class="text-md font-semibold text-gray-800 dark:text-white">
                                     {{ __('Channel') }} {{ $channelIndex + 1 }}
                                 </h4>
                                 <button type="button" wire:click="removeChannel({{ $channelIndex }})"
                                     class="text-red-500 hover:text-red-700 dark:hover:text-red-400">
-                                    <i class="fas fa-times-circle mr-1"></i>
-                                    {{ __('Remove channel') }}
+                                    <i class="fas fa-times-circle mr-1"></i> {{ __('Remove channel') }}
                                 </button>
                             </div>
                             <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -74,9 +74,7 @@ use App\Enums\ChannelReviewer;
                                     },
                                     get filteredChannels() {
                                         if (this.search === '') return this.channels;
-                                
                                         const term = this.search.toLowerCase();
-                                
                                         return this.channels.filter(c =>
                                             (c.name.toLowerCase().includes(term)) ||
                                             (c.number.toString().includes(term)) ||
@@ -98,35 +96,30 @@ use App\Enums\ChannelReviewer;
                                                 '{{ __('Search channel...') }}'"
                                             @focus="open = true" @input="if (search === '') clearSelection()"
                                             @click.away="open = false"
-                                            class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 pl-14 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                        <div
-                                            class="absolute left-3 top-1/2 transform -translate-y-1/2 flex items-center space-x-2">
+                                            class="w-full p-2.5 pl-14 rounded-lg bg-gray-50 border border-gray-300 dark:bg-gray-700 dark:text-white focus:ring-primary-600 focus:border-primary-600">
+                                        <div class="absolute left-3 top-1/2 -translate-y-1/2 flex items-center">
                                             <img x-show="selectedChannelImage" :src="selectedChannelImage"
-                                                class="w-8 h-8 object-contain object-center transition-opacity duration-200"
-                                                x-cloak>
+                                                class="w-8 h-8 object-contain object-center" x-cloak>
                                         </div>
-                                        <i class="fa-solid fa-chevron-down absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 dark:text-gray-300 cursor-pointer transition-transform duration-200"
+                                        <i class="fa-solid fa-chevron-down absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 dark:text-gray-300 cursor-pointer"
                                             :class="open ? 'rotate-180' : ''" @click="open = !open"></i>
                                     </div>
                                     <div class="relative">
                                         <div x-show="open" x-cloak
-                                            class="absolute z-10 mt-1 w-full max-w-md bg-white border border-gray-300 rounded-lg shadow-2xl dark:bg-gray-700 dark:border-gray-600 transition-all duration-200 ease-in-out">
+                                            class="absolute z-10 w-full mt-1 bg-white border rounded-lg shadow-2xl dark:bg-gray-700 transition-all">
                                             <ul
-                                                class="max-h-60 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100 dark:scrollbar-thumb-gray-600 dark:scrollbar-track-gray-700">
+                                                class="max-h-60 overflow-y-auto scrollbar-thin dark:scrollbar-thumb-gray-600">
                                                 <template x-for="channel in filteredChannels" :key="channel.id">
                                                     <li @click="
-                                                            $wire.set('reportData.channels.{{ $channelIndex }}.channel_id', channel.id);
-                                                            selectedChannelImage = channel.image;
-                                                            selectedChannelNumber = channel.number;
-                                                            selectedChannelName = channel.name;
-                                                            search = selectedChannelNumber + ' ' + selectedChannelName;
-                                                            open = false;
-                                                        "
-                                                        class="cursor-pointer px-4 py-2 flex items-center space-x-3 hover:bg-gray-100 dark:hover:bg-gray-600 transition-all duration-200 ease-in-out">
-                                                        <img :src="channel.image"
-                                                            class="w-10 h-10 object-contain object-center">
-                                                        <span
-                                                            class="text-sm font-medium text-gray-900 dark:text-gray-300"
+                                                        $wire.set('reportData.channels.{{ $channelIndex }}.channel_id', channel.id);
+                                                        selectedChannelImage = channel.image;
+                                                        selectedChannelNumber = channel.number;
+                                                        selectedChannelName = channel.name;
+                                                        search = selectedChannelNumber + ' ' + selectedChannelName;
+                                                        open = false;"
+                                                        class="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 cursor-pointer">
+                                                        <img :src="channel.image" class="w-10 h-10 object-contain">
+                                                        <span class="ml-3 text-sm text-gray-900 dark:text-gray-300"
                                                             x-text="channel.number + ' ' + channel.name"></span>
                                                     </li>
                                                 </template>
@@ -179,7 +172,7 @@ use App\Enums\ChannelReviewer;
                                         @endforeach
                                     </select>
                                 </div>
-                                <div class="col-span-2">
+                                <div class="col-span-1 md:col-span-2">
                                     <label class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
                                         <i class="fa-solid fa-comment mr-1.5"></i>
                                         {{ __('Description (Optional)') }}
@@ -192,9 +185,8 @@ use App\Enums\ChannelReviewer;
                         </div>
                     @endforeach
                     <button type="button" wire:click="addChannel"
-                        class="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-500 flex items-center gap-2">
-                        <i class="fas fa-plus-circle"></i>
-                        {{ __('Add channel') }}
+                        class="flex items-center gap-2 text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-500 transition">
+                        <i class="fas fa-plus-circle"></i> {{ __('Add channel') }}
                     </button>
                 </div>
                 <div class="my-6">
@@ -212,14 +204,13 @@ use App\Enums\ChannelReviewer;
                         @endforeach
                     </select>
                 </div>
-                <div class="flex justify-end gap-4">
+                <div class="flex flex-col md:flex-row justify-end gap-4 mt-6">
                     <button type="submit"
-                        class="py-2 px-4 bg-primary-600 hover:bg-primary-700 text-white rounded-lg shadow font-bold text-base">
-                        <i class="fas fa-file-lines mr-1.5"></i>
-                        {{ __('Generate report') }}
+                        class="w-full md:w-auto py-2 px-4 bg-primary-600 hover:bg-primary-700 text-white rounded-lg font-bold text-base">
+                        <i class="fas fa-file-lines mr-1.5"></i> {{ __('Generate report') }}
                     </button>
                     <button data-modal-hide="create-momently-report-modal" type="button"
-                        class="flex items-center gap-2 py-2 px-4 text-base font-bold text-gray-700 bg-white rounded-lg border border-gray-400 hover:border-primary-600 hover:text-primary-600 focus:ring-4 focus:ring-primary-200 dark:text-gray-300 dark:bg-gray-800 dark:border-gray-600 dark:hover:text-primary-400 dark:hover:bg-gray-700">
+                        class="py-2 px-4 text-base font-bold text-gray-700 bg-white rounded-lg border border-gray-400 hover:border-primary-600 hover:text-primary-600 dark:text-gray-300 dark:bg-gray-800 dark:border-gray-600 dark:hover:text-primary-400 dark:hover:bg-gray-700">
                         <i class="fa-solid fa-xmark"></i>
                         {{ __('Discard') }}
                     </button>
