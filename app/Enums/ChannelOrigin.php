182fca287bae5fa8cdead87c41c25232bbd85bd2
Edgar Leonel Acevedo Cuevas <edgarleonnels6x@gmail.com>
2025-05-03 14:23:43 -0600

    Refactor code structure for improved readability and maintainability

--- app/Enums/ChannelOrigin.php
+++ app/Enums/ChannelOrigin.php
@@ -5,11 +5,11 @@ namespace App\Enums;
 enum ChannelOrigin: string
 
 {
-    case KU_1 = 'KU 1';
-    case KU_3 = 'KU 3';
-    case KU_5 = 'KU 5';
-    case KU_7 = 'KU 7';
-    case KU_9 = 'KU 9';
+    case KU_01 = 'KU 01';
+    case KU_03 = 'KU 03';
+    case KU_05 = 'KU 05';
+    case KU_07 = 'KU 07';
+    case KU_09 = 'KU 09';
     case KU_11 = 'KU 11';
     case KU_13 = 'KU 13';
     case OVERON = 'OVERON';
