fbd303d56084f1d6be6b91c2474de3eee5416e3d
Edgar Leonel Acevedo Cuevas <edgarlsyxz@gmail.com>
2025-12-03 23:17:45 -0600

    fix: Comment out role-based section for master and admin in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -134,7 +134,7 @@
         @livewire('app.reports.report-momently-table')
     </div>
 
-    @role('master|admin')
+    {{-- @role('master|admin')
     <div class="w-full mt-6 px-4 mb-4 flex flex-col lg:flex-row gap-6">
         <div class="lg:w-3/4 w-full flex flex-col gap-6">
             <div class="flex flex-col md:flex-row gap-6">
@@ -159,7 +159,7 @@
             @livewire('app.logs.latest-logs')
         </div>
     </div>
-    @endrole
+    @endrole --}}
 
     <div id="create-momently-report-modal" tabindex="-1"
         class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
