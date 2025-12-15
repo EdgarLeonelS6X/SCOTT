acb3ff39b4fba2eb3d49891dd62cbd75129aa2d4
Edgar Leonel Acevedo Cuevas <edgarlsyxz@gmail.com>
2025-12-15 14:56:39 -0600

    feat: Add 'Monthly downloads' feature and improve device registration UI

--- resources/views/livewire/admin/devices/device-table.blade.php
+++ resources/views/livewire/admin/devices/device-table.blade.php
@@ -1,13 +1,20 @@
-<div class="bg-white dark:bg-gray-800 relative shadow-2xl rounded-lg overflow-hidden">
+<div class="relative shadow-2xl overflow-hidden">
     @can('create', App\Models\Device::class)
         <x-slot name="action">
-            <a href="{{ route('admin.devices.create') }}"
-                class="hidden sm:block text-white {{ Auth::user()?->area === 'DTH'
+            <div class="flex items-center space-x-3">
+                <a href="{{ route('admin.devices.create') }}"
+                    class="hidden sm:block text-white {{ Auth::user()?->area === 'DTH'
         ? 'bg-secondary-700 hover:bg-secondary-800 focus:ring-4 focus:ring-secondary-300 dark:bg-secondary-600 dark:hover:bg-secondary-700 dark:focus:ring-secondary-800'
         : 'bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800' }} font-medium rounded-lg text-sm px-5 py-2 focus:outline-none shadow-xl">
-                <i class="fa-solid fa-plus mr-1"></i>
-                {{ __('Register new device') }}
-            </a>
+                    <i class="fa-solid fa-plus mr-1"></i>
+                    {{ __('Register new device') }}
+                </a>
+                <a href=""
+                    class="hidden sm:block text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800 font-medium rounded-lg text-sm px-5 py-2 focus:outline-none shadow-xl">
+                    <i class="fa-solid fa-download mr-1"></i>
+                    {{ __('Monthly downloads') }}
+                </a>
+            </div>
         </x-slot>
         <a href="{{ route('admin.devices.create') }}"
             class="mb-4 sm:hidden block text-center text-white {{ Auth::user()?->area === 'DTH'
@@ -16,9 +23,14 @@
             <i class="fa-solid fa-plus mr-1"></i>
             {{ __('Register new device') }}
         </a>
+        <a href=""
+            class="mb-4 sm:hidden block text-center text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800 font-medium rounded-lg text-sm px-5 py-2 focus:outline-none shadow-xl">
+            <i class="fa-solid fa-download mr-1"></i>
+            {{ __('Monthly downloads') }}
+        </a>
     @endcan
 
-    <div class="overflow-x-auto">
+    <div class="overflow-x-auto rounded-lg">
         <table class="w-full table-fixed text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs dark:text-white uppercase dark:bg-gray-600 shadow-2xl">
                 <tr>
